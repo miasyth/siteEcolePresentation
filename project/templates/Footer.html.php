66faa9e4df8adcbbc4c97a0d101546f9d50ea4d1
BlayaMike <blaya.mike62@gmail.com>
2021-12-08 15:57:29 +0100

    Footer page php

---
+++ project/templates/Footer.html.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="footer_div">
+        <div class="presentation"><a href="">Présentation</a></div>
+        <div><a href="">Contact</a></div>
+        <div><a href="">Réseau</a></div>
+        <div class="mention"><a href="">Mentions légales</a></div>
+    </div>
+</footer>
