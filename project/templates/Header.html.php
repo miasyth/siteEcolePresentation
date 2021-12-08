1038738bcd9d67065f73d2f21f51cb10f2f53b48
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-08 15:49:46 +0100

    Twig, HTML, controllers & CSS

---
+++ project/templates/Header.html.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+
+    <a href="/"><img id="logoecole" src=".\img\logoecole.png"></a>
+
+    <ol>
+
+        <a class="Presentation" href="/presentation">Presentation</a>
+        <a class="Classes">Nos Classes</a>
+        <a class="Équipe pédagogique">Équipe pédagogique</a>
+        <a class="Connexion"><i class="fa fa-user" size:5x></i>Connexion</a>
+
+    </ol>
+
+
+</nav>
---
+++ project/templates/perm_content.html.php
@@ -0,0 +1,5 @@
+<div class="icon-bar">
+    <a href="https://www.facebook.com/ecolesaintemariedelapresentationarras/" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+    <a href="https://www.google.com/maps?ll=50.289873,2.776971&z=15&t=m&hl=fr&gl=FR&mapclient=embed&cid=15364531625531735332" class="map-marker" target="_blank"><i class="fa fa-map-marker"></i></a>
+    <a href="mailto:lapresentation@orange.fr" class="envelope" target="_blank"><i class="fa fa-envelope"></i></a>
+</div>
