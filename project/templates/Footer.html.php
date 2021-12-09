3e65594513f12b0015859cd8729ca57d54e99969
BlayaMike <blaya.mike62@gmail.com>
2021-12-09 13:18:21 +0100

    fix contact

--- project/templates/Footer.html.php
+++ project/templates/Footer.html.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer_div">
         <div class="presentation"><a href="">Présentation</a></div>
-        <div><a href="contact">Contact</a></div>
+        <div><a href="">Contact</a></div>
         <div><a href="">Réseau</a></div>
         <div class="mention"><a href="">Mentions légales</a></div>
     </div>
