e1ce56db8207b450ff6e01d208afe941e9c87e7e
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-13 13:01:18 +0100

    footer

--- project/templates/Footer.html.php
+++ project/templates/Footer.html.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer_div">
-        <div class="presentation"><a href="">Présentation</a></div>
+        <div class="presentation"><a href="/presentation">Présentation</a></div>
         <div><a href="">Contact</a></div>
         <div><a href="">Réseau</a></div>
         <div class="mention"><a href="">Mentions légales</a></div>
