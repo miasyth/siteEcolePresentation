a72bf5c65b2f0108b0f7dbab961038ee0d3fd4dc
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-14 16:23:28 +0100

    js update

--- project/src/Controller/ClasseController.php
+++ project/src/Controller/ClasseController.php
@@ -8,8 +8,104 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClasseController extends AbstractController
 {
-    #[Route('/classe', name: 'classe')]
+    #[Route('/TPS', name: 'tres_petite_section')]
     public function index(): Response
+    {
+        return $this->render('classe/TPS.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/classe', name: 'classe')]
+    public function TPS(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/PS-MS', name: 'petite_et_moyenne_section')]
+    public function PSMS(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/MS', name: 'petite_et_moyenne_section')]
+    public function MS(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/GS', name: 'petite_et_moyenne_section')]
+    public function GS(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CP_1', name: 'petite_et_moyenne_section')]
+    public function CP_1(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CP_2', name: 'petite_et_moyenne_section')]
+    public function CP_2(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CE1', name: 'petite_et_moyenne_section')]
+    public function CE1(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CE12', name: 'petite_et_moyenne_section')]
+    public function CE12(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CE2', name: 'petite_et_moyenne_section')]
+    public function CE2(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CM1', name: 'petite_et_moyenne_section')]
+    public function CM1(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CM12', name: 'petite_et_moyenne_section')]
+    public function CM12(): Response
+    {
+        return $this->render('classe/classe.html.twig', [
+            'controller_name' => 'ClasseController',
+        ]);
+    }
+
+    #[Route('/CM2', name: 'petite_et_moyenne_section')]
+    public function CM2(): Response
     {
         return $this->render('classe/classe.html.twig', [
             'controller_name' => 'ClasseController',
