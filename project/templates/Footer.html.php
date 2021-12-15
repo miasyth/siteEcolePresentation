7cbeb257ba6c6a0b54ef6438dddd790a393fc045
Lucas Merino <lucas.merino@outlook.fr>
2021-12-15 10:16:54 +0100

    boutton reseau ok

--- project/templates/Footer.html.php
+++ project/templates/Footer.html.php
@@ -2,7 +2,7 @@
     <div class="footer_div">
         <div class="presentation"><a href="/presentation">Présentation</a></div>
         <div><a href="">Contact</a></div>
-        <div><a href="">Réseau</a></div>
+        <div><a href="https://www.facebook.com/ecolesaintemariedelapresentationarras/">Réseau</a></div>
         <div class="mention"><a href="/mentionlegal">Mentions légales</a></div>
     </div>
 </footer>
