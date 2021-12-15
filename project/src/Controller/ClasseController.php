7ac3dec74dd2f59346f950e3016f284a61e7c4ef
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-15 11:39:25 +0100

    controller

--- project/src/Controller/ClasseController.php
+++ project/src/Controller/ClasseController.php
@@ -8,99 +8,89 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClasseController extends AbstractController
 {
-    #[Route('/classe/TPS', name: 'tres_petite_section')]
-    public function TPS(): Response
+    #[Route('/classe/{classe}', name: 'details classe')]
+    public function index(string $classe): Response
     {
-        return $this->render('classe/TPS.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
+        if($classe=="TPS"){
+            return $this->render('classe/TPS.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
+        
+        if($classe=="PS-MS"){
+            return $this->render('classe/PS-MS.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
 
-    #[Route('/classe/PS-MS', name: 'petite_et_moyenne_section')]
-    public function PSMS(): Response
-    {
-        return $this->render('classe/PS-MS.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
 
-    #[Route('/classe/MS', name: 'moyenne_section')]
-    public function MS(): Response
-    {
-        return $this->render('classe/MS.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
+        if($classe=="MS"){
+            return $this->render('classe/MS.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
 
-    #[Route('/classe/GS', name: 'grande_section')]
-    public function GS(): Response
-    {
-        return $this->render('classe/GS.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
 
-    #[Route('/classe/CP_1', name: 'cours_preparatoire1')]
-    public function CP_1(): Response
-    {
-        return $this->render('classe/CP_1.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
+        if($classe=="GS"){
+            return $this->render('classe/GS.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
 
-    #[Route('/classe/CP_2', name: 'cours_preparatoire2')]
-    public function CP_2(): Response
-    {
-        return $this->render('classe/CP_2.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
 
-    #[Route('/classe/CE1', name: 'cours_elementaire_1')]
-    public function CE1(): Response
-    {
-        return $this->render('classe/CE1.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
+        if($classe=="CP_1"){
+            return $this->render('classe/CP_1.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
 
-    #[Route('/classe/CE12', name: 'cours_elementaire_1_et_2')]
-    public function CE12(): Response
-    {
-        return $this->render('classe/CE12.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
 
-    #[Route('/classe/CE2', name: 'cours_elementaire_2')]
-    public function CE2(): Response
-    {
-        return $this->render('classe/CE2.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
+        if($classe=="CP_2"){
+            return $this->render('classe/CP_2.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
 
-    #[Route('/classe/CM1', name: 'cours_moyens_1')]
-    public function CM1(): Response
-    {
-        return $this->render('classe/CM1.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
 
-    #[Route('/classe/CM12', name: 'cours_moyens_1_et_2')]
-    public function CM12(): Response
-    {
-        return $this->render('classe/CM12.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
-    }
+        if($classe=="CE1"){
+            return $this->render('classe/CE1.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
 
-    #[Route('/classe/CM2', name: 'cours_moyens_2')]
-    public function CM2(): Response
-    {
-        return $this->render('classe/CM2.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
+
+        if($classe=="CE12"){
+            return $this->render('classe/CE12.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
+
+
+        if($classe=="CE2"){
+            return $this->render('classe/CE2.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
+
+
+        if($classe=="CM1"){
+            return $this->render('classe/CM1.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
+
+
+        if($classe=="CM12"){
+            return $this->render('classe/CM12.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
+
+
+        if($classe=="CM2"){
+            return $this->render('classe/CM2.html.twig', [
+                'controller_name' => 'ClasseController',
+            ]);
+        }
     }
 }
