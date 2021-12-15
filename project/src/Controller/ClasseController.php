50f8008fa597dc7e1835546a5cc69cbc9ac457ca
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-15 17:28:47 +0100

    classe pages dynamiques

--- project/src/Controller/ClasseController.php
+++ project/src/Controller/ClasseController.php
@@ -11,66 +11,119 @@ class ClasseController extends AbstractController
     #[Route('/classe/{class}', name: 'details classe')]
     public function index(string $class): Response
     {
+        $color1="#40a4bc"; #maternelle
+        $color2="#82b442"; #cours preparatoires
+        $color3="#ea7d44"; #cours elementaire
+        $color4="#b1434a"; #cours moyens
+
         if($class=="TPS"){
-            $className="classe de tres petite section";
+
+            $className="tres petite section";
+            $color = $color1;
             $img1="TPBlultheau\BlultheauTP3.jpg";
             $img2="TPBlultheau\BlultheauTP5.jpg";
             $img3="TPBlultheau\BlultheauTP6.jpg";
+            $listeProfs="Mme Blutheau";
+
         } else if ($class=="PS-MS"){
-            $className="classe de petite et moyenne section";
+
+            $className="petite et moyenne section";
+            $color = $color1;
             $img1="PSMSCandellier\PSCandellier2.jpg";
             $img2="PSMSCandellier\PSCandellier3.jpg";
             $img3="PSMSCandellier\PSCandellier5.jpg";
+            $listeProfs="Mmes Candellier, Guffroy et Delphine";
+
         } else if ($class=="MS"){
-            $className="classe de moyenne section";
+
+            $className="moyenne section";
+            $color = $color1;
             $img1="MSMartinage\Martinage2.jpg";
             $img2="MSMartinage\Martinage3.jpg";
             $img3="MSMartinage\Martinage5.jpg";
+            $listeProfs="Mme Martinage et Melle Derieux";
+
         } else if ($class=="GS"){
-            $className="classe de grande section";            
+
+            $className="grande section";            
+            $color = $color1;
             $img1="GSBaudry\GSBaudry1.jpg";
             $img2="GSBaudry\GSBaudry3.jpg";
             $img3="GSBaudry\GSBaudry5.jpg";
+            $listeProfs="Mme Baudry";
+
         } else if ($class=="CP_1"){
-            $className="classe de CP 1";
+
+            $className="CP 1";
+            $color = $color2;
             $img1="CPCaille\CPCaille1.jpeg";
             $img2="CPCaille\CPCaille4.jpeg";
             $img3="CPCaille\CPCaille5.jpeg";
+            $listeProfs="Mme Camier";
+
         } else if ($class=="CP_2"){
-            $className="classe de CP 2";
+
+            $className="CP 2";
+            $color = $color2;
             $img1="CPCamier\CPCamier1.jpg";
             $img2="CPCamier\CPCamier2.jpg";
             $img3="CPCamier\CPCamier5.jpg";
+            $listeProfs="Mme Caillierez";
+
         } else if ($class=="CE1"){
-            $className="classe de CE1";
+
+            $className="CE1";
+            $color = $color3;
             $img1="CE1Dourlens\Ce1Dourlens1.jpg";
             $img2="CE1Dourlens\Ce1Dourlens2.jpg";
             $img3="CE1Dourlens\Ce1Dourlens4.jpg";
+            $listeProfs="Mme Dourlens";
+
         } else if ($class=="CE12"){
-            $className="classe de CE1 et CE2";
+
+            $className="CE1 et CE2";
+            $color = $color3;
             $img1="CE1CE2Dartois\Ce1Dartois1.jpg";
             $img2="CE1CE2Dartois\Ce1Dartois2.jpg";
             $img3="CE1CE2Dartois\Ce1Dartois6.jpg";
+            $listeProfs="Mme Dartois";
+
         } else if ($class=="CE2"){
-            $className="classe de CE2";
+
+            $className="CE2";
+            $color = $color3;
             $img1="CE2Tilloy\Ce2Tilloy1.jpg";
             $img2="CE2Tilloy\Ce2Tilloy2.jpg";
             $img3="CE2Tilloy\Ce2Tilloy3.jpg";
+            $listeProfs="Mmes Tilloy et Desbusquois";
+
         } else if ($class=="CM1"){
-            $className="classe de CM1";
+
+            $className="CM1";
+            $color = $color4;
             $img1="CM1Bosswue\CM1Bossuwe.jpg";
             $img2="CM1Bosswue\CM1Bossuwe1.jpg";
             $img3="CM1Bosswue\CM1Bossuwe2.jpg";
+            $listeProfs="M Bosswue";
+
         } else if ($class=="CM12"){
-            $className="classe de CM1 et CM2";
+
+            $className="CM1 et CM2";
+            $color = $color4;
             $img1="CM1CM2DuhaDup\CM12DuhaDup1.jpg";
             $img2="CM1CM2DuhaDup\CM12DuhaDup2.jpg";
             $img3="CM1CM2DuhaDup\CM12DuhaDup5.jpg";
+            $listeProfs="Mmes Dupont et Duhamel";
+
         } else if ($class=="CM2"){
-            $className="classe de CM2";
+
+            $className="CM2";
+            $color = $color4;
             $img1="CM2Cazer\CM2HeilCazer1.jpg";
             $img2="CM2Cazer\CM2HeilCazer2.jpg";
             $img3="CM2Cazer\CM2HeilCazer3.jpg";
+            $listeProfs="Mmes Cazer et Heil";
+
         } else {
             return $this->redirectToRoute('classes');
         }
@@ -81,6 +134,8 @@ class ClasseController extends AbstractController
             'img1' => $img1,
             'img2' => $img2,
             'img3' => $img3,
+            'color' => $color,
+            'listeProfs' => $listeProfs
         ]);
     }
 }
