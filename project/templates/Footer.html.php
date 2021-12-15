f9b5b249724d3a5a82f9dc884506a9c915838ccd
MaximilienHeaps <you@example.com>
2021-12-15 09:48:57 +0100

    Mention Légal exemple

--- project/templates/Footer.html.php
+++ project/templates/Footer.html.php
@@ -3,6 +3,6 @@
         <div class="presentation"><a href="/presentation">Présentation</a></div>
         <div><a href="">Contact</a></div>
         <div><a href="">Réseau</a></div>
-        <div class="mention"><a href="">Mentions légales</a></div>
+        <div class="mention"><a href="/mentionlegal">Mentions légales</a></div>
     </div>
 </footer>
