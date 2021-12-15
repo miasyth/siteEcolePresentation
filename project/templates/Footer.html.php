c44042da70a7187c334b1846375c85b0fba5e66a
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-15 11:45:23 +0100

    footer

--- project/templates/Footer.html.php
+++ project/templates/Footer.html.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer_div">
         <div class="presentation"><a href="/presentation">Présentation</a></div>
-        <div><a href="#formulaire_contact">Contact</a></div>
+        <div><a href="/accueil#formulaire_contact">Contact</a></div>
         <div><a href="https://www.facebook.com/ecolesaintemariedelapresentationarras/">Réseau</a></div>
         <div class="mention"><a href="/mentionlegal">Mentions légales</a></div>
     </div>
