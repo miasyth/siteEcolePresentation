7df6b5a62ea7f673bb3dc424f65e849b4ed3fc5a
BlayaMike <blaya.mike62@gmail.com>
2021-12-23 12:53:39 +0100

    Accueil carrousel 3 images

--- project/src/Controller/AccueilController.php
+++ project/src/Controller/AccueilController.php
@@ -11,8 +11,19 @@ class AccueilController extends AbstractController
     #[Route('/accueil', name: 'accueil')]
     public function index(): Response
     {
+        $img[0]="Facade\Façade1.jpeg";
+        $img[1]="LogoEcole\logoecole.png";
+        $img[2]="SalleMotricite\SalleMotricite1.jpg";
+        $img[3]="SalleRestauration\Maternelle\cantinePetits2.jpg";
+        $img[4]="SalleRestauration\Primaire\cantine1.jpg";
+        $img[5]="Garderie\garderie4.jpg";
+        $img[6]="CoursRecre\Maternelle\CoursMaternelle3.jpg";
+        $img[7]="CoursRecre\Primaire\CoursPrimaire1.jpeg";
+        $img[8]="Biblio\Biblio1.jpeg";
+
         return $this->render('/accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'img' => $img,
         ]);
     }
 
