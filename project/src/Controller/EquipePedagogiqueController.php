48f069c2e526108d85e2accb3f849af56742290a
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-29 12:41:33 +0100

    modularisation page equipe peda

--- project/src/Controller/EquipePedagogiqueController.php
+++ project/src/Controller/EquipePedagogiqueController.php
@@ -11,8 +11,17 @@ class EquipePedagogiqueController extends AbstractController
     #[Route('/equipe_pedagogique', name: 'equipe_pedagogique')]
     public function index(): Response
     {
+        $imgEquipe='equipePeda.jpeg';
+        $rangs=[
+            "Mme CUVELLIER Pauline (AESH), Mme DAUSSE Emelina AESH, Mme DOURLENS CE1, Mme BAUDRY GS, Mme CAMIER GS/CP, Mme CANDELIER PS/MS, Mme BULTEAU TPS/PS, 
+            Mr BOSSUWE CM, Mme HEIL CM2, Mme Marie Christine (secrétaire).",
+            "Mme JOLIBOIS Camille ( ASEM), Mme MERCIER  Isabelle (ASEM), Melle GUFFROY  PS-MS, Mme MARTINAGE MS , Mme ALISSE Chef d’établissement, 
+            Mme DARTOIS CE1/CE2, Mme TILLOY CE2 , Mme DUHAMEL CM1/CM2 , Mme CAILLIERIEZ CP, Mme BUISIERE Delphine (ASEM)."
+        ];
+
         return $this->render('equipe_pedagogique/equipe_pedagogique.html.twig', [
-            'controller_name' => 'EquipePedagogiqueController',
+            'imgEquipe' => $imgEquipe,
+            'rangs' => $rangs
         ]);
     }
 }
