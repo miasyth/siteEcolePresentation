5dd8797e628b2b6fec99bdb1b3c369484023866b
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-12-29 13:15:52 +0100

    modularisation page presentation

--- project/src/Controller/PresentationController.php
+++ project/src/Controller/PresentationController.php
@@ -11,8 +11,22 @@ class PresentationController extends AbstractController
     #[Route('/presentation', name: 'presentation')]
     public function index(): Response
     {
+        $img=[
+            "CoursRecre\Maternelle\CoursMaternelle1.jpg",
+            "SalleRestauration\Maternelle\cantinePetits2.jpg",
+            "SalleRestauration\Primaire\cantine1.jpg",
+            "MobiliterReduite\mobilité.png",
+            "SalleMotricite\SalleMotricite1.jpg"
+        ];
+        $reglement="reglement_interieur_de_lecole_sainte_marie_de_la_presentation.pdf";
+        $projetEducatif="Projet_éducatif.pdf";
+        $voletPastoral="MUST ADD WHEN RECEIVED";
+
         return $this->render('/presentation/presentation.html.twig', [
-            'controller_name' => 'PresentationController',
+            'img' => $img,
+            'reglement' => $reglement,
+            'projetEducatif' => $projetEducatif,
+            'voletPastoral' => $voletPastoral,
         ]);
     }
 }
