7a532e9a5345891324de40e06b8c332a89b18062
Corentin Fouquet <corentinfouquet62@gmail.com>
2022-01-10 12:09:55 +0100

    I hate WEB dev even more!!!

--- project/src/Controller/AccueilController.php
+++ project/src/Controller/AccueilController.php
@@ -8,6 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
+    #[Route('/accueil', name: 'accueil')]
+    public function index(): Response
+    {
+        $img=[
+            "Facade\Façade1.jpeg",
+            "LogoEcole\logoecole.png",
+            "SalleMotricite\SalleMotricite1.jpg",
+            "Classes\TPSPSBulteau\TPSPS_7.jpg",
+            "SalleRestauration\Primaire\cantine1.jpg",
+            "Garderie\garderie4.jpg",
+            "CoursRecre\Maternelle\CoursMaternelle3.jpg",
+            "CoursRecre\Primaire\CoursPrimaire1.jpeg",
+            "CoursRecre\Primaire\CoursPrimaire5.jpg",
+            "EntreEcoleCP\Facebook.jpg",
+            "Biblio\Biblio1.jpeg"
+        ];
+
+        return $this->render('/accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+            'img' => $img,
+        ]);
+    }
+
     #[Route('/', name: 'accueil')]
     public function indexAux(): Response
     {
