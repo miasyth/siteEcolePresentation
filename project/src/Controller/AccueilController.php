eb225f35ca637974e43864ecbefb0bd088116bb8
Corentin Fouquet <corentinfouquet62@gmail.com>
2022-01-14 11:24:49 +0100

    routes fix

--- project/src/Controller/AccueilController.php
+++ project/src/Controller/AccueilController.php
@@ -31,26 +31,9 @@ class AccueilController extends AbstractController
         ]);
     }
 
-    #[Route('/', name: 'accueil')]
+    #[Route('/', name: 'accueil_aux')]
     public function indexAux(): Response
     {
-        $img=[
-            "Facade\Façade1.jpeg",
-            "LogoEcole\logoecole.png",
-            "SalleMotricite\SalleMotricite1.jpg",
-            "Classes\TPSPSBulteau\TPSPS_7.jpg",
-            "SalleRestauration\Primaire\cantine1.jpg",
-            "Garderie\garderie4.jpg",
-            "CoursRecre\Maternelle\CoursMaternelle3.jpg",
-            "CoursRecre\Primaire\CoursPrimaire1.jpeg",
-            "CoursRecre\Primaire\CoursPrimaire5.jpg",
-            "EntreEcoleCP\Facebook.jpg",
-            "Biblio\Biblio1.jpeg"
-        ];
-
-        return $this->render('/accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-            'img' => $img,
-        ]);
+        return $this->index();
     }
 }
