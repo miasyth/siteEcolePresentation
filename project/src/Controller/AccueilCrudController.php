c2f3ea236aba0b7bf0453293062f126b8dc69e77
BlayaMike <blaya.mike62@gmail.com>
2022-01-19 12:27:20 +0100

    CRUD admin dashboard

---
+++ project/src/Controller/AccueilCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Accueil;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class AccueilCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Accueil::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
--- project/src/Controller/AdminController.php
+++ project/src/Controller/AdminController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class AdminController extends AbstractDashboardController
 {
     #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(AccueilCrudController::class)->generateUrl();
+        
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -24,7 +28,6 @@ class AdminController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoRoute('Home', 'fas fa-home', 'accueil');
     }
 }
