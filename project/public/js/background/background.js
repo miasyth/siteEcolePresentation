1e8f37037191afcab4073e60bb2389ad4395698b
MaximilienHeaps <you@example.com>
2022-03-09 21:40:11 +0100

    Date change selon temperature

--- project/public/js/background/background.js
+++ project/public/js/background/background.js
@@ -1,9 +1,9 @@
 const today = new Date();
 const date = {jour:today.getDate(),mois:(today.getMonth()+1)};
-const spring = {jour:20,mois:3};
+const spring = {jour:01,mois:3};
 const summer = {jour:21,mois:6};
 const autumn = {jour:23,mois:9};
-const winter = {jour:21,mois:12};
+const winter = {jour:15,mois:12};
 
 if(date.jour>=spring.jour && date.mois==spring.mois || date.mois>spring.mois && date.mois<summer.mois || date.jour<summer.jour && date.mois==summer.mois){
     //printemps
